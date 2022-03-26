cad8e8a43e3f648a2e213dc5d815c87ebafb8119
Kaia Scheirman <kfscheirman18@gmail.com>
2022-03-25 17:52:52 -0700

    add all other required packages

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-// TODO: Include packages needed for this application
+// import required packages 
 const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
 const questions = [];
